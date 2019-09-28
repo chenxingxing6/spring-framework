666f6d7f967290bc8e007b6cf1b680865f19f1ec
lanxinghua <lanxinghua@2dfire.com>
2019-09-28 15:03:55 +0800

    readme

---
+++ my-test/src/main/java/com/demo/Main.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+/**
+ * User: lanxinghua
+ * Date: 2019/9/28 14:43
+ * Desc:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("****");
+    }
+}
--- my-test/src/test/java/com/demo/MyTest.java
+++ my-test/src/test/java/com/demo/MyTest.java
@@ -1,5 +1,8 @@
 package com.demo;
 
+
+import com.alibaba.fastjson.JSON;
+
 /**
  * User: lanxinghua
  * Date: 2019/9/27 14:16
@@ -7,6 +10,7 @@ package com.demo;
  */
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("**");
+        System.out.println("***");
+        System.out.println(JSON.toJSONString("1111"));
     }
 }
